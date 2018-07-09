16d259b5013fb5242fe457ebc6d949fe0c49b49c
Joos Wesemael <joos.wesemael@crossroad.be>
2018-07-09 23:45:00 +0200

    first real commit proj start

---
+++ cnn_mnist.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Imports
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Our application logic will be added here
+
+def main(unused_argv):
+    print("hello world")
+
+
+if __name__ == "__main__":
+  tf.app.run()
